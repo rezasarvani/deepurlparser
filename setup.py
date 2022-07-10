ae6af145691b7f38a2bb2b0677d2ba1eebbd7a78
rezasarvani <49445406+rezasarvani@users.noreply.github.com>
2022-07-10 23:22:13 +0430

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='deepurlparser',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/rezasarvani/deepurlparser',
+    license='',
+    author='Reza Sarvani',
+    author_email='rezasarvani70@gmail.com',
+    description='A wrapper around \'tldextract\' library in order to access different part of URL more easily.'
+)
